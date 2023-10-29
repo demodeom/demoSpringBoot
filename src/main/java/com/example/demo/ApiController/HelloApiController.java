deee3fbb0b26021b9dd00b8ef52ad89a4da36141
demodeom <demodeom@outlook.com>
2023-10-29 11:04:36 +0800

    接收请求参数、参数默认值

--- src/main/java/com/example/demo/ApiController/HelloApiController.java
+++ src/main/java/com/example/demo/ApiController/HelloApiController.java
@@ -2,6 +2,7 @@ package com.example.demo.ApiController;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -11,12 +12,23 @@ import java.util.Map;
 public class HelloApiController {
 
     @GetMapping("/api/hello")
-    public Map<String, Object> hello()
-    {
+    public Map<String, Object> hello() {
         Map<String, Object> res = new HashMap<>();
         res.put("code", 0);
         res.put("message", "success");
         res.put("data", "Hello Restfull Api");
+        return res;
+    }
+
+    @GetMapping("/api/hi")
+    public Map<String, Object> hi(
+            @RequestParam(name = "name", defaultValue = "Tony") String name,
+            @RequestParam(name = "age", defaultValue = "18") Integer age
+    ) {
+        Map<String, Object> res = new HashMap<>();
+        res.put("code", 0);
+        res.put("message", "success");
+        res.put("data", "姓名：" + name + ", 年龄: " + age);
         return res;
     }
 
